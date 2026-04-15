8f2d298e3593b8dbee28c30014adba9e5445fcff
Sumeet Parmar <shipit@users.noreply.github.com>
2026-04-14 20:57:29 -0500

    feature: android dockerfile generator

---
+++ generator/src/main/kotlin/tools/kaiju/gradlezilla/generator/DockerfileGenerator.kt
@@ -0,0 +1,7 @@
+package tools.kaiju.gradlezilla.generator
+
+class DockerfileGenerator : Generator {
+    override fun generate(spec: GeneratorSpec): String {
+        TODO("Not yet implemented")
+    }
+}
