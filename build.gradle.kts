ca48d195f31a07b8168e8d51a88724685383b305
Sumeet Parmar <shipit@users.noreply.github.com>
2026-04-15 13:10:18 -0500

    added ktlint and detekt configs

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,24 @@ allprojects {
         mavenCentral()
     }
 }
+
+subprojects {
+    apply(
+        plugin =
+            rootProject.libs.plugins.ktlint
+                .get()
+                .pluginId,
+    )
+    apply(
+        plugin =
+            rootProject.libs.plugins.detekt
+                .get()
+                .pluginId,
+    )
+
+    // Tell Kotlin explicitly what type of extension to configure
+    extensions.configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        config.setFrom(files("$rootDir/detekt-config.yaml"))
+        buildUponDefaultConfig = true
+    }
+}
